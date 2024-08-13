f7ca83a1c0babea59a2ca941b1b2e03851add52f
HansMoleman <aceind13@aol.com>
2024-08-13 20:08:13 -0230

    Continued implementing out command center for the 'hash' command. The user must always enter the input (as bit or text) and the key (as text), but may also enter the input as a file (either .txt or .bin) and/or output results to file(.bin) or to screen (bit or text). As of right now the only combination tested and confirmed working is when the input is passed as text or bits, with a text key, and the output is printed to screen. All other scenarios must still be tested.

--- prototype/crypu.py
+++ prototype/crypu.py
@@ -37,28 +37,87 @@ def main():
 	argc = len(sys.argv)
 
 	if 1 < argc:
-
 		if sys.argv[1] == "hash":
 			path_in = ""
 			data_in = ""
+			type_in = ""
 			key_in  = ""
 			path_out = ""
 			data_out = ""
 
-			if 3 < argc:
-				if argc == 4:
-					# input data (text or bits) + key (text)
-					raw_data = sys.argv[2]
-					key_in   = sys.argv[3]
+			if 3 < argc and argc < 8:
 
+				if argc == 4:
+					# no options passed (input data + key only)
+					raw_data = sys.argv[2]
 					if isBitString(raw_data):
 						data_in = raw_data
-						print("already bits")
+						type_in = "bin"
 					else:
-						# convert to bit string
-						print("needs converting")
+						data_in = stringToBinary(raw_data)
+						type_in = "txt"
+					key_in = stringToBinary(sys.argv[3])
+
+				elif argc == 5:
+					# file input option passed
+					if sys.argv[2] == "-f":
+						path_in = sys.argv[3]
+						key_in = stringToBinary(sys.argv[4])
+					else:
+						print("ERROR: weirdness...")
+
+				elif argc == 6:
+					# file output option passed
+					raw_data = sys.argv[2]
+					if isBitString(raw_data):
+						data_in = raw_data
+						type_in = "bin"
+					else:
+						data_in = stringToBinary(raw_data)
+						type_in = "txt"
+					key_in = stringToBinary(sys.argv[3])
+
+					if sys.argv[4] == "-f":
+						path_out = sys.argv[5]
+					else:
+						print("ERROR: more weirdness")
+
+				elif argc == 7:
+					# file input and file output options passed
+					if sys.argv[2] == "-f":
+						path_in = sys.argv[3]
+						key_in = stringToBinary(sys.argv[4])
+
+						if sys.argv[5] == "-f":
+							path_out = sys.argv[6]
+						else:
+							print("ERROR: inner weirdness")
+					else:
+						print("ERROR: outer weirdness")
+
+				if data_in == "":
+					if path_in[-3:] == "txt":
+						fobj = open(path_in, 'r')
+						txt_in = fobj.read()
+						fobj.close()
+						data_in = stringToBinary(txt_in)
+						type_in = "txt"
+					elif path_in[-3:] == "bin":
+						data_in = loadHash(path_in)
+						type_in = "bin"
+					else:
+						print("ERROR: incompatible file types detected...")
+
+				data_out = xorHash(data_in, key_in)
+				if path_out != "":
+					saveHash(path_out, data_out)
+					print(f"hash data saved to {path_out}.")
 				else:
-					print("options not implemented yet...")
+					if type_in == "bin":
+						print(binaryToString(data_out))
+					else:
+						print(data_out)
+					
 			
 			else:
 				if sys.argv[2] == "--help":
