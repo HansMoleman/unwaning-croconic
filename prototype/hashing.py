4ea68fc4065295c756e23d08c5d2c7435966ab79
HansMoleman <aceind13@aol.com>
2024-08-20 21:04:21 -0230

    Added and implemented 'messageLength' method, and added and began implementing the 'md5Hash' method. Up to and including breaking down the input message into 512-bit blocks is implemented, and the case where the input message is less than 448 bits, resulting in a single block of (padded) 512 bits, has been tested and is working. The method overall is not yet implemented enough to be functional.

--- prototype/hashing.py
+++ prototype/hashing.py
@@ -74,3 +74,86 @@ def saveHash(filepath, bitstring):
 		fobj.close()
 	except:
 		print("ERROR: could not open and/or write to target file; please check passed filepath and try again.")
+
+
+## md5Hash(str) : str
+#
+# Computes the MD5 hash of the given input message (bitstring) and
+# returns the corresponding 128-bit hash.
+##
+def md5Hash(bitstr_in):
+	block_size = 512
+	lbits_size = 64
+
+	# pad to 512-bits, or break down into chunks of 512-bits
+	chunks = []
+	if len(bitstr_in) < (512 - 64):
+		# one chunk
+		vchunk = ""
+		for i in range(len(bitstr_in)):
+			vchunk += bitstr_in[i]
+
+		vchunk += '1'
+		while(len(vchunk) < (512 - 64)):
+			vchunk += '0'
+
+		vchunk += messageLength(bitstr_in)
+
+		if len(vchunk) == block_size:
+			chunks.append(vchunk)
+		else:
+			print(f"INTERNAL ERROR: MD5 hashing requires a block size of 512 bits, but have {len(vchunk)} bits. Exiting...")
+			exit(1)
+
+	else:
+		# multiple chunks
+		offset = 0
+		chunk = ""
+		for i in range(len(bitstr_in)):
+			if offset < (block_size - lbits_size):
+				chunk += bitstr_in[i]
+				offset += 1
+			else:
+				chunk_size = messageLength(chunk)
+				chunk += '1'
+				chunk += chunk_size
+				if len(chunk) == block_size:
+					chunks.append(chunk)
+				else:
+					print(f"INTERNAL ERROR: MD5 hashing requires a block size of 512 bits, but have {len(chunk)} bits. Exiting...")
+					exit(1)
+
+				offset = 0
+				chunk = ""
+				chunk += bitstr_in[i]
+				offset += 1
+
+		if 0 < offset:
+			size_chunk = messageLength(chunk)
+			chunk += '1'
+			while(len(chunk) < (block_size - lbits_size)):
+				chunk += '0'
+			chunk += size_chunk
+			chunks.append(chunk)
+
+	print(chunks)
+
+	# break message block down into 16 x 32-bit words
+	pass
+
+
+## messageLength(str) : str
+#
+# Measures the length of the passed message and converts the size from
+# decimal to binary, and returns the binary string padded to 64 bits.
+##
+def messageLength(msg_bitstr):
+	vsize = len(msg_bitstr)
+	bsize = bin(vsize)[2:]
+
+	size_bitstr = ""
+	while((len(size_bitstr) + len(bsize)) < 64):
+		size_bitstr += '0'
+	size_bitstr += bsize
+
+	return size_bitstr
