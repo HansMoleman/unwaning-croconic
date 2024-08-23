08ca76db4a9f38cc9cf5d1f43770a93cf0aec494
HansMoleman <aceind13@aol.com>
2024-08-23 14:34:38 -0230

    Revised the 'typedef unsigned long int UINT4' line to work on 64-bit machines and included stdint.h for the uint32_t type, as recommended by a StackOverflow answer (linked in file). The md5driver system now works as expected, and will produce output of -x option that matches the RFC1321 document.

--- prototype/rfc1321/global.h
+++ prototype/rfc1321/global.h
@@ -6,6 +6,9 @@
 The following makes PROTOTYPES default to 0 if it has not already
   been defined with C compiler flags.
  */
+
+#include <stdint.h>
+
 #ifndef PROTOTYPES
 #define PROTOTYPES 0
 #endif
@@ -17,7 +20,8 @@ typedef unsigned char *POINTER;
 typedef unsigned short int UINT2;
 
 /* UINT4 defines a four byte word */
-typedef unsigned long int UINT4;
+//typedef unsigned long int UINT4;
+typedef uint32_t UINT4;
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
