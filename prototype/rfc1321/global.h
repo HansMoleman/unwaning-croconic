3196f6a50ba818e45cf677e649d9af0ab3157bd2
HansMoleman <aceind13@aol.com>
2024-08-23 14:37:22 -0230

    Added link to StackOverflow answer that worked.

--- prototype/rfc1321/global.h
+++ prototype/rfc1321/global.h
@@ -22,6 +22,7 @@ typedef unsigned short int UINT2;
 /* UINT4 defines a four byte word */
 //typedef unsigned long int UINT4;
 typedef uint32_t UINT4;
+// https://stackoverflow.com/a/697397
 
 /* PROTO_LIST is defined depending on how PROTOTYPES is defined above.
 If using PROTOTYPES, then PROTO_LIST returns the list, otherwise it
